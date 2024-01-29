93d496624da2dc12e496601fe472f2dca43024e7
Satya Suranjeet Jena <102950648+Satyasuranjeet@users.noreply.github.com>
2024-01-29 14:52:56 +0530

    Update ExpensesFilter.js
    modified the code to work with some new value

--- Client/src/components/Expenses/ExpensesFilter.js
+++ Client/src/components/Expenses/ExpensesFilter.js
@@ -12,6 +12,9 @@ const ExpensesFilter = (props) => {
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value='2026'>2026</option>
+          <option value='2025'>2025</option>
+          <option value='2024'>2024</option>
           <option value='2023'>2023</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
