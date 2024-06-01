fd89d07c4e6443b8f561610db4870b5d22e5415a
Satya Suranjeet Jena <102950648+Satyasuranjeet@users.noreply.github.com>
2024-06-01 15:41:36 +0530

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -3,7 +3,7 @@ let dbConnection
 
 module.exports={
     connectToDB: (cb) => {
-        MongoClient.connect("mongodb+srv://satya:Satya12345@cluster0.8thgg4a.mongodb.net/?retryWrites=true&w=majority")
+        MongoClient.connect("mongodb+srv://satya:satya@cluster0.8thgg4a.mongodb.net/?retryWrites=true&w=majority")
         .then((client) =>{
             dbConnection = client.db('ExpenseTracker')
             return cb()
